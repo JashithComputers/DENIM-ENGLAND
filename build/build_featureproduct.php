0095d5bcf33cce73a075bbd7b906d9060706cabc
Vijayakumar Moorthy <vijay@ventunotech.com>
2016-12-15 18:54:59 +0530

    map added

---
+++ build/build_featureproduct.php
@@ -0,0 +1,47 @@
+<?php
+require_once 'config.php';
+require_once 'products_lib.php';
+
+$product_dir = __BASE_PATH . DS . "product";
+$products = scandir($product_dir);
+
+foreach ($products as $product)
+{
+	if(substr($product, 0,1)=='.') continue;
+
+	$curProductDir = $product_dir . DS . $product;
+
+	if(is_dir($curProductDir))
+	{
+		echo "\n\n";
+		echo '<div class="sl-feature-product">';
+		echo "\n\n";
+		$subItems = scandir($curProductDir);
+		
+		foreach ($subItems as $subItem)
+		{
+			if(substr($subItem, 0,1)=='.') continue;
+				
+			$curSubItemDir = $curProductDir . DS . $subItem;
+				
+			if(file_exists($curSubItemDir . DS . "info.json"))
+			{
+				$info = file_get_contents($curSubItemDir . DS . "info.json");
+				$info = json_decode($info);
+					
+				foreach ($info->images as $img)
+				{
+					echo "\n";
+					$imgSrc = "http://static.denimengland.com/product/{$product}/{$subItem}/build/images_200/{$img}";
+					
+					echo '<img src="'.$imgSrc.'" />';
+				}
+			}
+		}
+		echo "\n\n";
+		echo "</div>";
+		echo "\n\n";
+	}
+}
+
+echo "\n";
