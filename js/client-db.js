40d96745e459e8ec4c8e3a366a10746d89253d83
Vijayakumar Moorthy <vijay@ventunotech.com>
2016-12-24 11:44:40 +0530

    updates

--- js/client-db.js
+++ js/client-db.js
@@ -1,42 +1,80 @@
-JCL_firebase.setup({
-	DB_DOMAIN:"denimengland-com"
-});
-
-$(document).ready(function(){
-	client_setupHeader();
-});
-
-function client_setupHeader()
-{
-	if(JCL_firebase.is_auth())
-	{
-		var welcomemsg = "Welcome "+JCL_firebase.getName();
-		$("#jcHeader #topstrip #headerwelcomemsg").html(welcomemsg);
+	JCL_firebase.setup({
+		DB_DOMAIN:"denimengland-com"
+	});
+	
+	function googleLogin(){
+		var nextURL = document.referrer || "/";
+		
+		$(document).ready(function(){
+			JCL_firebase.googleLogin(function(user){
+				console.log(user);
+				window.top.location = nextURL;
+			},function(error){
+				console.log(error);
+			});
+		});
 	}
-}
-
-function googleLogin(){
-	var nextURL = document.referrer || "/";
+	
+	function facebookLogin(){
+		var nextURL = document.referrer || "/";
+		
+		$(document).ready(function(){
+			JCL_firebase.facebookLogin(function(user){
+				console.log(user);
+				window.top.location = nextURL;
+			},function(error){
+				console.log(error);
+			});
+		});
+	}
+	
+	function client_setupHeader()
+	{
+		if(JCL_firebase.is_auth())
+		{
+			var welcomemsg = "Welcome "+JCL_firebase.getName();
+		}
+	}
+	
+	function registerAuthCallback(callback)
+	{
+		window._JCL_firebase_onAuthStateChanged = window. _JCL_firebase_onAuthStateChanged || [];
+		window._JCL_firebase_onAuthStateChanged.push(callback);
+		
+		var obj = callback;
+		var key = Object.keys(obj)[0];
+		if(typeof(callback[key])=="function")
+			callback[key]();
+	}
+	
+	function fireAuthCallbacks()
+	{
+		window._JCL_firebase_onAuthStateChanged = window. _JCL_firebase_onAuthStateChanged || [];
+		
+		var authFiredKeys = []; 
+		for (x in window._JCL_firebase_onAuthStateChanged)
+		{
+			var obj = window._JCL_firebase_onAuthStateChanged[x];
+			var key = Object.keys(obj)[0];
+			if(typeof(authFiredKeys[key])=="undefined"){
+				authFiredKeys[key] = key;
+				console.log(key);
+				if(typeof(obj[key])=="function")
+					obj[key]();
+			}
+			else{
+				consolr.log("already fired");
+			}
+		}
+	}
+	
 	
 	$(document).ready(function(){
-		JCL_firebase.googleLogin(function(user){
-			console.log(user);
-			window.top.location = nextURL;
-		},function(error){
-			console.log(error);
+		
+		registerAuthCallback({client_setupHeader:client_setupHeader});
+		
+		JCL_firebase.onAuthStateChanged(function(user){
+			fireAuthCallbacks();
 		});
 	});
-}
-
-function facebookLogin(){
-	var nextURL = document.referrer || "/";
 	
-	$(document).ready(function(){
-		JCL_firebase.facebookLogin(function(user){
-			console.log(user);
-			window.top.location = nextURL;
-		},function(error){
-			console.log(error);
-		});
-	});
-}
